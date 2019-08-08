afc589937c1ee83de4ce915e7ee028532dc873fb
Iaroslav Sokolov <sokolov.yas@gmail.com>
2019-08-08 23:51:13 +0300

    Refactor code and implement information extracting from filename

--- prepare_git_data.py
+++ prepare_git_data.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 from random import shuffle
 from typing import Tuple, Generator
 
+import typing
 from tqdm import tqdm
 
 MIN_LENGTH = 5
@@ -33,15 +34,17 @@ def filter_content(content: str) -> str:
     return content
 
 
-def get_filename_content(path: str) -> Tuple[str, str]:
-    filename = os.path.basename(path)
+def get_filename_and_content(path: str) -> Tuple[str, str]:
+    filename = os.path.basename(path).split('_')
     with open(path, "rt", encoding="utf-8", errors="ignore") as f:
         content = f.read()
-    return filename, content
+    repo_name = filename[1]
+    example_path = '/'.join(filename[2:])
+    return ' '.join((repo_name, example_path)), content
 
 
 def merge_filename_content(filename: str, content: str) -> str:
-    return f"<<!<<{filename}>>!>>\n{content}{SPLIT_SYMBOL}"
+    return f"{SPLIT_SYMBOL}<<!<<{filename}>>!>>\n{content}"
 
 
 def get_train_file_paths(parent_path: str) -> Generator[str, None, None]:
@@ -56,6 +59,32 @@ def get_test_file_paths(parent_path: str) -> str:
     return os.path.join(parent_path, f"test.txt")
 
 
+def close_file(file_object: typing.TextIO) -> None:
+    file_object.write(SPLIT_SYMBOL)
+    file_object.close()
+
+
+def write_examples(file_path: str, examples_iterator: iter, file_size: int) -> bool:
+    file_object = open(file_path, "wt", encoding="utf-8")
+    cur_file_size = 0
+    for example_path in examples_iterator:
+        filename, content = get_filename_and_content(example_path)
+        if filename_is_good(filename) and content_is_good(content):
+            filename, content = filter_filename(filename), filter_content(content)
+            to_write = merge_filename_content(filename, content)
+            len_content = len(content)
+            cur_file_size += len_content
+
+            file_object.write(to_write)
+            cur_file_size += len_content
+
+            if cur_file_size >= file_size:
+                close_file(file_object)
+                return True
+    close_file(file_object)
+    return False
+
+
 def main(file_path_pattern: str, dataset_path: str) -> None:
     source_file_paths = glob.glob(file_path_pattern)
     assert source_file_paths, f"Nothing found at {file_path_pattern}"
@@ -63,74 +92,16 @@ def main(file_path_pattern: str, dataset_path: str) -> None:
     source_file_paths, files_total_len = iter(source_file_paths), len(source_file_paths)
 
     train_paths = get_train_file_paths(dataset_path)
-    valid_file = open(get_valid_file_paths(dataset_path), "wt", encoding="utf-8")
-    test_file = open(get_test_file_paths(dataset_path), "wt", encoding="utf-8")
-    train_file = open(next(train_paths), "wt", encoding="utf-8")
-
-    tr_count, all_count = 0, 0
-    train_files_count = 1
-    cur_train_size, cur_test_size, cur_valid_size, all_size = 0, 0, 0, 0
-    min_size, max_size = float("+inf"), float("-inf")
 
     # Write to valid
-    for file_path in source_file_paths:
-        all_count += 1
-        filename, content = get_filename_content(file_path)
-        if filename_is_good(filename) and content_is_good(content):
-            filename, content = filter_filename(filename), filter_content(content)
-            filename_content = merge_filename_content(filename, content)
-            len_content = len(content)
-            all_size += len_content
-
-            valid_file.write(filename_content)
-            cur_valid_size += len_content
-
-            if cur_valid_size >= VAL_FILE_SIZE:
-                valid_file.close()
-                break
+    write_examples(get_valid_file_paths(dataset_path), source_file_paths, VAL_FILE_SIZE)
 
     # Write to test
-    for file_path in source_file_paths:
-        all_count += 1
-        filename, content = get_filename_content(file_path)
-        if filename_is_good(filename) and content_is_good(content):
-            filename, content = filter_filename(filename), filter_content(content)
-            filename_content = merge_filename_content(filename, content)
-            len_content = len(content)
-            all_size += len_content
-
-            test_file.write(filename_content)
-            cur_test_size += len_content
-
-            if cur_test_size >= TEST_FILE_SIZE:
-                test_file.close()
-                break
+    write_examples(get_test_file_paths(dataset_path), source_file_paths, TEST_FILE_SIZE)
 
     # Write to train
-    for file_path in tqdm(source_file_paths, total=files_total_len - all_count):
-        all_count += 1
-        filename, content = get_filename_content(file_path)
-        if filename_is_good(filename) and content_is_good(content):
-            tr_count += 1
-            filename = filter_filename(filename)
-            content = filter_content(content)
-            filename_content = merge_filename_content(filename, content)
-            len_content = len(content)
-            all_size += len_content
-            min_size, max_size = min(min_size, len_content), max(max_size, len_content)
-
-            train_file.write(filename_content)
-            cur_train_size += len_content
-
-            if cur_train_size >= TRAIN_FILE_SIZE:  # assume that each symbol takes 1 byte
-                cur_train_size = 0
-                train_file.close()
-                train_file = open(next(train_paths), "wt", encoding="utf-8")
-                train_files_count += 1
-    train_file.close()
-
-    print(f"{tr_count} of {all_count} examples have been filtered and wrote to {train_files_count} files ~100MB each\n"
-          f"min/average/max lengths of filtered examples are {min_size}/{all_size / tr_count}/{max_size} symbols")
+    while write_examples(next(train_paths), source_file_paths, TRAIN_FILE_SIZE):
+        pass
 
 
 if __name__ == "__main__":
