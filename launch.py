3a0d7754b2661197fdd2fd189dae9eb98d49bf50
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-09-14 18:40:21 -0700

    change back to git02 image since git03 doesn’t exist

--- launch.py
+++ launch.py
@@ -50,7 +50,7 @@ args = parser.parse_args()
 
 # default environment settings, should change rarely since they affect
 # all configs
-IMAGE_NAME = 'cybertronai01_git03'
+IMAGE_NAME = 'cybertronai01_git02'
 # CONDA_ENV = 'pytorch_april_nccl237'
 CONDA_ENV = 'pytorch_p36'
 
