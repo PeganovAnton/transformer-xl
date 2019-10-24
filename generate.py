c0cfd5c141d4100861d828e817f67a2685764ad3
Iaroslav Sokolov <sokolov.yas@gmail.com>
2019-10-24 19:03:56 +0300

    Reformat generate.py

--- generate.py
+++ generate.py
@@ -16,35 +16,31 @@ from util import unwrap_model
 
 
 def main():
-    parser = argparse.ArgumentParser(description='PyTorch Transformer Language Model')
-    parser.add_argument('--model_path', type=str, required=True,
-                        help='path to the model weights checkpoint')
-    parser.add_argument('--dataset', type=str, required=True,
-                        help='The dataset on which the model was trained')
-    parser.add_argument('--top_k', type=int, default=0,
-                        help='Limit sampling to top K probabilities. If 0, use all.')
-    parser.add_argument('--top_p', type=float, default=0,
-                        help='Limit sampling to p nucleus sampling. If 0, use all.')
-    parser.add_argument('--length', type=int, default=200,
-                        help='what sequence length to generate')
-    parser.add_argument('--batch_size', type=int, default=10)
-    parser.add_argument('--batch_len', type=int, default=384)
+    parser = argparse.ArgumentParser(description="PyTorch Transformer Language Model")
+    parser.add_argument("--model_path", type=str, required=True, help="path to the model weights checkpoint")
+    parser.add_argument("--dataset", type=str, required=True, help="The dataset on which the model was trained")
+    parser.add_argument("--top_k", type=int, default=0, help="Limit sampling to top K probabilities. If 0, use all.")
+    parser.add_argument("--top_p", type=float, default=0, help="Limit sampling to p nucleus sampling. If 0, use all.")
+    parser.add_argument("--length", type=int, default=200, help="what sequence length to generate")
+    parser.add_argument("--batch_size", type=int, default=10)
+    parser.add_argument("--batch_len", type=int, default=384)
     parser.add_argument("--temperature", type=float, default=1.0)
     # Only for wiki dataset
-    parser.add_argument('--context', type=str, default='',
-                        help='Conditional generation context')
+    parser.add_argument("--context", type=str, default="", help="Conditional generation context")
     # Only for git dataset
-    parser.add_argument('--cur_file', type=str, default=None,
-                        help='Only for git dataset. Conditional generation context')
-    parser.add_argument('--context_files', type=str, default=None, nargs='+',
-                        help='Only for git dataset. Rest project files')
+    parser.add_argument(
+        "--cur_file", type=str, default=None, help="Only for git dataset. Conditional generation context"
+    )
+    parser.add_argument(
+        "--context_files", type=str, default=None, nargs="+", help="Only for git dataset. Rest project files"
+    )
 
     args = parser.parse_args()
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load the best saved model.
-    with open(args.model_path, 'rb') as f:
-        model = torch.load(f, map_location='cuda' if torch.cuda.is_available() else 'cpu')
+    with open(args.model_path, "rb") as f:
+        model = torch.load(f, map_location="cuda" if torch.cuda.is_available() else "cpu")
     model = unwrap_model(model)
 
     if not torch.cuda.is_available():
@@ -59,11 +55,12 @@ def main():
     else:
         assert False, f"function for context preparation not implemented for dataset {args.dataset}"
 
-    generated_text = generate_text(model, context, args.length, args.batch_size,
-                                   args.temperature, args.top_k, args.top_p, args.batch_len)
+    generated_text = generate_text(
+        model, context, args.length, args.batch_size, args.temperature, args.top_k, args.top_p, args.batch_len
+    )
 
     for i in range(len(generated_text)):
-        print('=' * 40, 'sample', i + 1, '=' * 40)
+        print("=" * 40, "sample", i + 1, "=" * 40)
         print(generated_text[i])
 
 
@@ -98,14 +95,14 @@ def generate_text(
     model.eval()
     if not tokenizer:
         from pytorch_pretrained_bert import GPT2Tokenizer
-        tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     context = tokenizer.encode(context)
     terminal_id = tokenizer.encode("\n")
     assert len(terminal_id) == 1
     terminal_id = terminal_id[0]
 
     with torch.no_grad():
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         data = torch.tensor(context).to(device)
         # Turn into a batch.
         data = data.unsqueeze(1)
