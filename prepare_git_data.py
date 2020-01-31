d508998162195e0fddcceb6bb90218ac537a307e
Iaroslav Sokolov <sokolov.yas@gmail.com>
2020-01-31 17:44:33 +0300

    Support data preparation for new BPE format. Support option for project-level datasets. More verbosity and improved architecture

--- prepare_git_data.py
+++ prepare_git_data.py
@@ -5,160 +5,306 @@ import json
 import os
 import random
 from argparse import ArgumentParser
-from typing import Tuple, Iterable
+from typing import Tuple, Iterable, Iterator, List, Union
 
 import tqdm
-
-MIN_LENGTH = 5
-
-PROJECT_SPLIT_SYMBOL = "\n龖龖龖\n"
-FILE_SPLIT_SYMBOL = "\n!龖!\n"
-
-TRAIN_FILE_SIZE = 100 * (1000 ** 2)  # Convert 100MB into bytes
-VAL_FILE_SIZE = 1 * (1000 ** 2)
-TEST_FILE_SIZE = 1 * (1000 ** 2)
-
-SEED = None
+from pygments.lexers.python import Python3Lexer
 
 
-def prepare_project(project: Iterable[Tuple[str, str]], cur_file: Tuple[str, str] = None) -> str:
-    def filter_project(project: Iterable[Tuple[str, str]]) -> Iterable[Tuple[str, str]]:
-        def filename_is_good(filename: str) -> bool:
-            return True
+class GitDataPreprocessor(object):
+    def __init__(
+        self,
+        *,
+        min_content_len: int,
+        example_split_symbol: str,
+        project_level: bool,
+        file_split_symbol: str = None,
+        filepath_split_symbol: str = "",
+    ):
+        assert (
+            len(example_split_symbol) == 1
+        ), f"You must provide a symbol, not a string. {example_split_symbol} were provided"
+        assert (
+            len(filepath_split_symbol) == 1
+        ), f"You must provide a symbol, not a string. {filepath_split_symbol} were provided"
+        assert (
+                len(file_split_symbol) == 1
+        ), f"You must provide a symbol, not a string. {file_split_symbol} were provided"
 
-        def content_is_good(content: str) -> bool:
-            if len(content) < MIN_LENGTH:
-                return False
-            return True
+        if project_level:
+            assert file_split_symbol is not None, "If you want project level dataset you must provide file split symbol"
+            self._file_split_symbol = file_split_symbol + "\n"
+        else:
+            self._file_split_symbol = ""
+        self._min_content_len = min_content_len
+        self._example_split_symbol = example_split_symbol
+        self._project_level = project_level
+        self._path_content_split_symbol = filepath_split_symbol
 
-        return (
-            (filename, content)
-            for filename, content in project
-            if filename_is_good(filename) and content_is_good(content)
-        )
+        self._py_lexer = Python3Lexer()
 
-    def preprocess_project(project: Iterable[Tuple[str, str]]) -> Iterable[Tuple[str, str]]:
-        def preprocess_filename(filename: str) -> str:
-            return filename
-
-        def preprocess_content(content: str) -> str:
-            return content
-
-        return ((preprocess_filename(filename), preprocess_content(content)) for filename, content in project)
-
-    def project_to_string(project: Iterable[Tuple[str, str]]) -> str:
-        def merge_path_content(path: str, content: str) -> str:
-            return f"{FILE_SPLIT_SYMBOL}<<!<<{path}>>!>>\n{content}"
-
-        text = PROJECT_SPLIT_SYMBOL
-        text += "".join(merge_path_content(path, content) for path, content in project)
+    def prepare_context(self, context_files: Iterable[Tuple[str, str]], file_to_complete: Tuple[str, str]) -> str:
+        text = f"{self._example_split_symbol}\n"
+        if self._project_level:
+            text += "".join((self._prepare_file(file, turn_filter=True) for file in context_files))
+        text += self._prepare_file(file_to_complete, turn_filter=False)
         return text
 
-    project = itertools.chain(project, (cur_file,) if cur_file else tuple())
-    project = filter_project(project)
-    project = preprocess_project(project)
-    project = project_to_string(project)
-    return project
-
-
-def write_file(file_path: str, texts: Iterable[str], file_size: int = None) -> bool:
-    if not file_size:
-        file_size = float("+inf")
-    file_object = open(file_path, "wt", encoding="utf-8")
-    cur_file_size = 0
-
-    for text in texts:
-        cur_file_size += file_object.write(text)
-
-        if cur_file_size >= file_size:
-            file_object.close()
-            return True
-    file_object.close()
-    return False
-
-
-def get_projects_v3(raw_data_path: str, languages: Iterable[str] = ("Python",)) -> Iterable[Iterable[Tuple[str, str]]]:
-    def read_file(path):
-        with open(path, "rt", encoding="utf-8", errors="ignore") as f:
-            return f.read()
-
-    random.seed(SEED)
-
-    files = []
-    for language in languages:
-        files += glob.iglob(os.path.join(raw_data_path, "v3", "languages", language, ".*", "**", "*.*"), recursive=True)
-    assert files, f"There are no files in {raw_data_path} with languages {languages}"
-
-    projects = collections.defaultdict(list)
-    for file in files:
-        if os.path.isfile(file):
-            project_name = "/".join(file.split("/")[-3:-1])
-            projects[project_name].append(file)
-
-    projects = list(projects.items())
-    random.shuffle(projects)
-    print(f"Found {len(projects)} projects")
-
-    total_bar = tqdm.tqdm()
-    total_bar.set_description("Projects was processed")
-
-    for project_name, files in projects:
-        paths_dict_path = os.path.join(
-            raw_data_path,
-            "v3",
-            "repositories",
-            files[0].split("/")[5],
-            files[0].split("/")[6],
-            files[0].split("/")[7],
-            "paths.json",
+    def prepare_example(self, example: Union[Iterable[Tuple[str, str]], Tuple[str, str]]) -> str:
+        project_level_example = not isinstance(example, Tuple)
+        assert self._project_level == project_level_example, (
+            "If GitDataPreprocessor instance is for project level dataset, you must provide Iterable[Tuple[str, str]], "
+            "and Tuple[str, str] otherwise"
         )
-        with open(paths_dict_path, "rt") as f:
-            paths_dict = json.load(f)
-        paths_and_contents = (
-            (paths_dict[os.path.basename(file)], read_file(file))
-            for file in files
-            if os.path.basename(file) in paths_dict
+        if self._project_level:
+            example_text = "".join((self._prepare_file(e, turn_filter=True) for e in example))
+        else:
+            example_text = self._prepare_file(example, turn_filter=True)
+        return f"{self._example_split_symbol}\n{example_text}" if example_text != "" else ""
+
+    def string_to_examples(self, string: str) -> Union[Iterable[Iterable[Tuple[str, str]]], Iterable[Tuple[str, str]]]:
+        examples = string.split(self._example_split_symbol)
+        if self._project_level:
+            return map(
+                lambda s: map(
+                    lambda e: tuple(e.split(self._path_content_split_symbol)), s.split(self._file_split_symbol)
+                ),
+                examples,
+            )
+        else:
+            return map(lambda e: tuple(e.split(self._path_content_split_symbol)), examples)
+
+    def _prepare_file(self, file: Tuple[str, str], turn_filter: bool) -> str:
+        if not turn_filter or (self._filename_is_good(file[0]) and self._content_is_good(file[1])):
+            return self._merge_path_content(self._preprocess_filename(file[0]), self._preprocess_content(file[1]))
+        else:
+            return ""
+
+    def _filename_is_good(self, filename: str) -> bool:
+        return bool(filename)
+
+    def _content_is_good(self, content: str) -> bool:
+        return len(content) >= self._min_content_len
+
+    def _preprocess_filename(self, filename: str) -> str:
+        return filename
+
+    def _preprocess_content(self, content: str) -> str:
+        # tokens = self._py_lexer.get_tokens_unprocessed(content)
+        # return "".join(tok[2] for tok in tokens if not is_token_subtype(str(tok[1]), Comment))
+        return content
+
+    def _merge_path_content(self, path: str, content: str) -> str:
+        return f"{self._file_split_symbol}{path}\n{self._path_content_split_symbol}\n{content}\n"
+
+
+class GitDataExtractor(object):
+    def __init__(
+        self,
+        *,
+        min_content_len: int,
+        example_split_symbol: str,
+        project_level: bool,
+        file_split_symbol: str = None,
+        filepath_split_symbol: str = "",
+        num_projects_to_load: int = None,
+        random_seed: int = None,
+    ):
+        self._project_level = project_level
+        self._num_projects_to_load = num_projects_to_load if num_projects_to_load is not None else float("+inf")
+        self._random_seed = random_seed
+
+        self._content_set = set()
+        self._write_counter = 0
+
+        self._preprocessor = GitDataPreprocessor(
+            min_content_len=min_content_len,
+            example_split_symbol=example_split_symbol,
+            project_level=project_level,
+            file_split_symbol=file_split_symbol,
+            filepath_split_symbol=filepath_split_symbol,
         )
-        total_bar.update(1)
-        yield paths_and_contents
 
+    def prepare_dataset(
+        self,
+        archive_path: str,
+        target_folder_path: str,
+        train_files_size: int = 100,
+        val_file_size: int = 1,
+        test_file_size: int = 1,
+    ):
+        def get_train_file_paths(parent_path: str) -> Iterator[str]:
+            return (os.path.join(parent_path, f"train_{i}.txt") for i in itertools.count(start=1))
 
-def main(raw_data_path: str, dataset_path: str) -> None:
-    def get_train_file_paths(parent_path: str) -> Iterable[str]:
-        return (os.path.join(parent_path, f"git_{i}.txt") for i in itertools.count(1))
+        def get_valid_file_path(parent_path: str) -> str:
+            return os.path.join(parent_path, f"valid.txt")
 
-    def get_valid_file_paths(parent_path: str) -> str:
-        return os.path.join(parent_path, f"valid.txt")
+        def get_test_file_path(parent_path: str) -> str:
+            return os.path.join(parent_path, f"test.txt")
 
-    def get_test_file_paths(parent_path: str) -> str:
-        return os.path.join(parent_path, f"test.txt")
+        # MB -> Bytes
+        train_files_size *= 1000 ** 2
+        val_file_size *= 1000 ** 2
+        test_file_size *= 1000 ** 2
 
-    projects = get_projects_v3(raw_data_path)
+        projects = self._get_projects_v3(archive_path)
 
-    projects = map(prepare_project, projects)
+        self._write_texts(projects, get_valid_file_path(target_folder_path), val_file_size)
+        self._write_texts(projects, get_test_file_path(target_folder_path), test_file_size)
 
-    # Write to valid
-    write_file(get_valid_file_paths(dataset_path), projects, VAL_FILE_SIZE)
+        # Convert to file level if needed
+        examples = projects if self._project_level else self._shuffle_examples(projects)
 
-    # Write to test
-    write_file(get_test_file_paths(dataset_path), projects, TEST_FILE_SIZE)
+        train_paths = get_train_file_paths(target_folder_path)
+        while self._write_texts(examples, next(train_paths), train_files_size):
+            pass
 
-    # Write to train
-    train_paths = get_train_file_paths(dataset_path)
+    def _prepare_example(self, example: Union[Tuple[str, str], Iterator[Tuple[str, str]]]) -> str:
+        example_is_project = not isinstance(example, Tuple)
+        if example_is_project and not self._project_level:
+            text = "".join(self._preprocessor.prepare_example(file) for file in example)
+        else:
+            text = self._preprocessor.prepare_example(example)
+        text_hash = hash(text)
+        if text_hash in self._content_set:
+            return ""
+        else:
+            self._content_set.add(text_hash)
+            return text
 
-    while write_file(next(train_paths), projects, TRAIN_FILE_SIZE):
-        pass
+    def _write_texts(
+        self,
+        examples: Union[Iterator[Tuple[str, str]], Iterator[Iterator[Tuple[str, str]]]],
+        file_path: str,
+        file_size: int = None,
+    ) -> bool:
+
+        if not file_size:
+            file_size = float("+inf")
+
+        with open(file_path, "wt", encoding="utf-8") as file_object:
+            cur_file_size = 0
+
+            for example in examples:
+                text = self._prepare_example(example)
+                cur_file_size += file_object.write(text)
+
+                self._writing_bar.update(1)
+                if cur_file_size >= file_size:
+                    return True
+            return False
+
+    def _shuffle_examples(self, projects: Iterator[Iterator[Tuple[str, str]]]) -> Iterator[Tuple[str, str]]:
+        def fetch_n(src: Iterator[Iterator[Tuple[str, str]]], n: int) -> Tuple[List[Iterator[Tuple[str, str]]], bool]:
+            items = []
+            for i, example in enumerate(src):
+                items.append(example)
+                if i >= n:
+                    return items, True
+            return items, False
+
+        has_next = True
+        while has_next:
+            examples, has_next = fetch_n(projects, self._num_projects_to_load)
+            examples = [file for project in examples for file in project]
+            random.seed(self._random_seed)
+            random.shuffle(examples)
+            yield from examples
+
+    def _get_projects_v3(
+        self, raw_data_path: str, languages: Iterable[str] = ("Python",)
+    ) -> Iterator[Iterator[Tuple[str, str]]]:
+        def read_file(path):
+            with open(path, "rt", encoding="utf-8", errors="ignore") as f:
+                return f.read()
+
+        files = []
+        for language in languages:
+            files += glob.iglob(
+                os.path.join(raw_data_path, "v3", "languages", language, ".*", "**", "*.*"), recursive=True
+            )
+        assert files, f"There are no files in {raw_data_path} with languages {languages}"
+
+        projects = collections.defaultdict(list)
+        for file in files:
+            if os.path.isfile(file):
+                project_name = "/".join(file.split("/")[-3:-1])
+                projects[project_name].append(file)
+
+        projects = list(projects.items())
+        projects_amount = len(projects)
+
+        processed_projects = []
+        files_amount = 0
+        for project_name, files in projects:
+            paths_dict_path = os.path.join(
+                raw_data_path,
+                "v3",
+                "repositories",
+                "repositories",
+                # files[0].split("/")[5],
+                files[0].split("/")[6],
+                files[0].split("/")[7],
+                "master",
+                "paths.json",
+            )
+            with open(paths_dict_path, "rt") as f:
+                paths_dict = json.load(f)
+            names_and_paths = [
+                (paths_dict[os.path.basename(file)], file) for file in files if os.path.basename(file) in paths_dict
+            ]
+            processed_projects.append(names_and_paths)
+            files_amount += len(names_and_paths)
+
+        print(f"Found {projects_amount} projects with {files_amount} files")
+
+        reading_bar = tqdm.tqdm(total=projects_amount, desc="Reading projects...")
+        self._writing_bar = (
+            tqdm.tqdm(total=projects_amount, desc="Processing and writing projects...")
+            if self._project_level
+            else tqdm.tqdm(total=files_amount, desc="Processing and writing files...")
+        )
+        random.seed(self._random_seed)
+        random.shuffle(processed_projects)
+        for project in processed_projects:
+            yield ((name, read_file(path)) for name, path in project)
+            reading_bar.update(1)
 
 
 if __name__ == "__main__":
     args = ArgumentParser()
-    args.add_argument("--raw_data_path", type=str, required=True, help="path to the dataset (v3) folder")
-    args.add_argument("--dataset_path", type=str, required=True, help="where to store train valid test files")
+    args.add_argument("--src", type=str, required=True, help="path to the dataset (v3) folder")
+    args.add_argument("--dest", type=str, required=True, help="where to store train valid test files")
+    # Optional arguments
     args.add_argument("--min_len", type=int, default=5, help="minimum file content length")
-    args.add_argument("--seed", type=int, default=None, help="random seed for example shuffle")
+    args.add_argument("--example_sym", type=str, default="␢", help="symbol which will be inserted between examples")
+    args.add_argument("--project_level", action="store_true", help="whether to build project level dataset")
+    args.add_argument(
+        "--file_sym", type=str, default="ℱ", help="symbol which will be inserted between files within project"
+    )
+    args.add_argument(
+        "--filepath_sym",
+        type=str,
+        default="₣",
+        help="symbol which will be inserted between filepath and content of file",
+    )
+    args.add_argument(
+        "--proj_memory", type=int, default=None, help="how many projects will be load to memory for random shuffle"
+    )
+    args.add_argument("--seed", type=int, default=17, help="random seed for example shuffle")
+    args.add_argument("--train_size", type=int, default=100, help="size of train files tat will be produced")
+    args.add_argument("--val_size", type=int, default=1, help="size of train files tat will be produced")
+    args.add_argument("--test_size", type=int, default=10, help="size of train files tat will be produced")
     args = args.parse_args()
 
-    MIN_LENGTH = args.min_len
-    SEED = args.seed
+    data_extractor = GitDataExtractor(
+        min_content_len=args.min_len,
+        example_split_symbol=args.example_sym,
+        project_level=args.project_level,
+        file_split_symbol=args.file_sym,
+        filepath_split_symbol=args.filepath_sym,
+        num_projects_to_load=args.proj_memory,
+        random_seed=args.seed,
+    )
 
-    main(args.raw_data_path, args.dataset_path)
+    data_extractor.prepare_dataset(args.src, args.dest, args.train_size, args.val_size, args.test_size)
