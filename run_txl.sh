643bcdbbf5328621e67d62c6af0cca0940e0253d
Ubuntu <ubuntu@ip-172-31-4-19.ec2.internal>
2020-03-08 14:44:28 +0000

    Add script for hf-txl training

--- run_txl.sh
+++ run_txl.sh
@@ -1,1 +1,25 @@
-#!/usr/bin/env bash
+#/bin/bash
+
+NCCL_MIN_NRINGS=16 NCCL_MAX_NRINGS=16 PYTHONPATH="." python hf_training/launch.py \
+	--model_type=txl \
+	--example_symbol= \
+	--train_data_file=../cooked_data/train_1.txt \
+	--output_dir=../cached_dir \
+	--tokenizer_path=../gitbpe-16384.bpe \
+	--bpe_dropout=0.1 \
+	--model_size=txl-like \
+	--warmup_tokens=50000000 \
+	--weight_decay=0.1 \
+	--do_train \
+	--do_eval \
+	--evaluate_during_training \
+	--per_gpu_train_batch_size=5 \
+	--logging_steps=500 \
+	--eval_data_file=../cooked_data/valid.txt \
+	--per_gpu_eval_batch_size=8 \
+	--save_steps=500 \
+	--num_train_epochs=5.0 \
+	--fp16 \
+	--fp16_opt_level O1 \
+	--overwrite_output_dir \
+	--should_continue
